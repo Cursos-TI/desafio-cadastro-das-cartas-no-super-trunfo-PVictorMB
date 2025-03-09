72330491005f9ae7c30b33663a8c86be16b71e8e
PVictorMB <victormendes73@gmail.com>
2025-03-09 10:02:51 +0000

    return 0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,81 +15,21 @@ int main(){
     char EstadoF;
     char EstadoG;
     char EstadoH;
-    char cidadeA01[20],cidadeA02[20],cidadeA03[20],cidadeA04[20];
-    char cidadeB01[20],cidadeB02[20],cidadeB03[20],cidadeB04[20];
-    char cidadeC01[20],cidadeC02[20],cidadeC03[20],cidadeC04[20];
-    char cidadeD01[20],cidadeD02[20],cidadeD03[20],cidadeD04[20];
-    char cidadeE01[20],cidadeE02[20],cidadeE03[20],cidadeE04[20];
-    char cidadeF01[20],cidadeF02[20],cidadeF03[20],cidadeF04[20];
-    char cidadeG01[20],cidadeG02[20],cidadeG03[20],cidadeG04[20];
-    char cidadeH01[20],cidadeH02[20],cidadeH03[20],cidadeH04[20];
+    char cidade1;
+    char cidade2;
+    char cidade3;
+    char cidade4;
     char CodigodaCarta[10];
     int população;
     float Áreakm²;
     float PIB;
     int Numerosdepontosturisticos;
     
-    printf("digite EstadoA:,\n");
-    scanf("%d",&EstadoA);
-
-    printf("digite cidadeA01:,\n");
-    scanf("%s",&cidadeA01);
-    printf("digite cidadeA02:,\n");
-    scanf("%d",&cidadeA02);
-    printf("digite cidadeA03,\n");
-    scanf("%d",&cidadeA03);
-    printf("digite cidadeA04,\n");
-    scanf("%d",&cidadeA04);
-
-    printf("digite Codigo da Carta:,\n)");
-    scanf("%d",&CodigodaCarta);
-
-    printf("digite população:,\n");
-    scanf("%f",&população);
-
-    printf("digite ÁreaKM²:,\n");
-    scanf("%f",&Áreakm²);
-
-    printf("digite PIB:,\n");
-    scanf("%f",&PIB);
-
-    printf("digite numeros de pontos turisticos:,\n");
-    scanf("%d",&Numerosdepontosturisticos);
-
-    printf("digite EstadoB:,\n");
-    scanf("%d",&EstadoB);
-
-    printf("digite cidadeB01,\n");
-    scanf("%d",&cidadeB01);
-    printf("digite cidadeB02,\n");
-    scanf("%d",&cidadeB02);
-    printf("digite cidadeB03,\n");
-    scanf("%d",&cidadeB03);
-    printf("digite cidadeB04,\n");
-    scanf("%d",&cidadeB04);
-
-    printf("digite EstadoC,\n");
-    scanf("%d",&EstadoC);
-
-    printf("digite cidadeC01,\n");
-    scanf("%d",&cidadeC01);
-    printf("digite cidadeC02,\n");
-    scanf("%d",&cidadeC02);
-    printf("digite a cidadeC03,\n");
-    scanf("%d",&cidadeC03);
 
 
 
 
-
-
-
-
-
-    
-
-
-
+system("pause");
     return 0;
 
     }
